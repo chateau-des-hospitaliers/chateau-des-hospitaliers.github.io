6015a18c3550b6c63ac42e9437f6e5ae2164d9f9
louismartinpierrat@gmail.com <louismartinpierrat@gmail.com>
2019-04-07 01:02:52 +0200

    Add gallery ; use bootstrap material design

--- javascript/main.js
+++ javascript/main.js
@@ -2,4 +2,21 @@
 
   "use strict";
 
+  $(window).on('load', function() {
+
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 200) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+      } else {
+        $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
+    });
+
+    // one page navigation
+    $('.navbar-nav').onePageNav({
+      currentClass: 'active'
+    });
+
+  });
+
 }(jQuery));
