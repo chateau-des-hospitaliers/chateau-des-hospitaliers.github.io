412403c18891922b943f1d2442fd733a3400587e
louismartinpierrat@gmail.com <louismartinpierrat@gmail.com>
2019-04-07 13:01:12 +0200

    Add map ; refactoring

--- javascript/main.js
+++ javascript/main.js
@@ -12,13 +12,20 @@
       }
     });
 
-    // one page navigation
-    $('.navbar-nav').onePageNav({
-      currentClass: 'active'
-    });
-
   });
 
   new WOW().init()
 
 }(jQuery));
+
+
+function init_map() {
+
+'use strict';
+
+  var map = new google.maps.Map(document.getElementById('map-container'), {
+    zoom: 15,
+    center: {lat: 43.735718, lng: 4.090071}
+  });
+
+};
